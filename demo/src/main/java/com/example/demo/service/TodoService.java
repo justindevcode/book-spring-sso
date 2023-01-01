ce45ed780b0842c6849e47bddbccca55da85313b
witwint <witwint1234@gmail.com>
2023-01-01 15:45:06 +0900

    벡엔드 서비스개발update service

--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
 import java.util.List;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,7 +23,11 @@ public class TodoService {
 	}
 
 	public List<TodoEntity> create(final TodoEntity entity) {
+		validate(entity);
 
+		repository.save(entity);
+		log.info("Entity Id : {} id saved.", entity.getId());
+		return repository.findByUserId(entity.getUserId());
 	}
 
 	private void validate(final TodoEntity entity) {
@@ -33,7 +38,27 @@ public class TodoService {
 
 		if(entity.getUserId() == null) {
 			log.warn("Unknown user.");
-			throw new RuntimeException("Unknoen user.");
+			throw new RuntimeException("Unknown user.");
 		}
+	}
+
+	public List<TodoEntity> retrieve(final String userId) {
+		return repository.findByUserId(userId);
+	}
+
+	public List<TodoEntity> update(final TodoEntity entity) {
+		validate(entity);
+
+		final Optional<TodoEntity> original = repository.findById(entity.getId());
+
+		original.ifPresent(todo ->{
+			todo.setTitle(entity.getTitle());
+			todo.setDone(entity.isDone());
+
+			repository.save(todo);
+
+		});
+
+		return retrieve(entity.getUserId());
 	}
 }
