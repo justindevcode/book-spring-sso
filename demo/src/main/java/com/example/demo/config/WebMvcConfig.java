0c668b6acbd8b4532c8167dd73c4ea4d1468c727
witwint <witwint1234@gmail.com>
2023-01-04 18:15:19 +0900

    CORS 백엔드 설정

---
+++ demo/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configurable
+public class WebMvcConfig implements WebMvcConfigurer {
+	private final long MAX_AGE_SECS = 3600;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		//모든 경로에 대해
+		registry.addMapping("/**")
+			//Origin이 로컬3000에 대해
+			.allowedOrigins("http://localhost:3000")
+			//GET,POST,PUT,PATCH,DELETE,OPTIONS 매서드 허용
+			.allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+			.allowedHeaders("*")
+			.allowCredentials(true)
+			.maxAge(MAX_AGE_SECS);
+	}
+
+}
