07ffb556c42c721de0ba06e7cc89b71b668ff792
witwint <witwint1234@gmail.com>
2023-01-08 20:22:40 +0900

    aws eb init

--- demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 			.authorizeRequests() // /와 /auth/** 경로는 인증 안해도됨.
-				.antMatchers("/","/auth/**").permitAll()
+				.antMatchers("/","/auth/**","/error").permitAll()
 			.anyRequest() // /와 /auth/**이외의 모든 경로는 인증해야됨.
 				.authenticated();
 
